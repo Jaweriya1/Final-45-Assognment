da01b1c36232077cf72ce1634b1c135ab794753a
[jaweriya aslam] <[jaweriyaaslam12@gmail.com]>
2024-06-02 22:06:55 +0500

    first commit

---
+++ assignment3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the famous quote and author
+let quote = "Nothing is impossible because I am possible";
+let author = "Hina Naseer";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ assignment3/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and author
+
+let quote: string = "Nothing is impossible because I am possible"
+
+let author: string = "Hina Naseer";
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
