9945f182bdad12441c4c5d74db6a2dbbfcf001a5
[jaweriya aslam] <[jaweriyaaslam12@gmail.com]>
2024-06-22 13:08:01 +0500

    [First commit]

---
+++ assignment 4/main.js
@@ -0,0 +1,6 @@
+var famQuote = "A person who never made a mistake never tried anything new.";
+var author = "Albert Einstein";
+console.log("".concat(author, " once said, \"").concat(famQuote));
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said, \"").concat(famQuote);
+console.log(message);
---
+++ assignment 4/main.ts
@@ -0,0 +1,8 @@
+let famQuote:string = "A person who never made a mistake never tried anything new."
+let author:string = "Albert Einstein";
+console.log(`${author} once said, "${famQuote}`);
+
+// assignment 5
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, "${famQuote}`
+console.log(message);
